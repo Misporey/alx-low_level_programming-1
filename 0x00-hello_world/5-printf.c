b29f630f1e8cf9bb7fd01d98fc2552b12305867f
david-kimengu <davidmutune200@gmail.com>
2020-09-16 18:22:04 +0000

    print to stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *Return:Returns 0 if no erros, and returns a non zero value if errors are found.
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+
+}
