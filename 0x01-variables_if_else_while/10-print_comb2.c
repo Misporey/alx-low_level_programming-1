152f3caa39a6cea12d7f7aac44026b4b8c26a345
david-kimengu <davidmutune200@gmail.com>
2020-09-17 11:38:24 +0000

    Right no of putchar fn added

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,18 +11,10 @@ int main(void)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			if (i == '9' && j == '9')
-			{
-				putchar(i);
-				putchar(j);
-			}
-			else
-			{
-				putchar(i);
-				putchar(j);
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
