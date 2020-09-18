b9a1ddcc0c2080a492567e02162ef33184078932
david-kimengu <davidmutune200@gmail.com>
2020-09-18 12:05:18 +0000

    Add last advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  *
  *Return: zero if no error, non-zero int if error.
  */
-int write(int filedes, const char *buf, unsigned int nbyte); 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-1960\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-1960\n");
+	return (1);
 
 }
