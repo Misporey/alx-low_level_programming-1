8ea81caf402a34c517a85fcc6314d9715a186fa6
david-kimengu <davidmutune200@gmail.com>
2020-09-18 12:02:00 +0000

    Add last advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *
+ *Return: zero if no error, non-zero int if error.
+ */
+int write(int filedes, const char *buf, unsigned int nbyte); 
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-1960\n");
+
+}
