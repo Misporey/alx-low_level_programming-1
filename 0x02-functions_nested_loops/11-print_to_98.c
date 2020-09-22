d948064a8045988b2afe829a9a8ede98b3f9f51b
david-kimengu <davidmutune200@gmail.com>
2020-09-22 10:19:52 +0000

    Fix betty style errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "holberton.h"
+
+void case_1(int);
+void case_2(int);
+void case_3(int);
 /**
  *print_to_98- prints numbers from given value to 98.
  *@n:number from which to print to 98.
@@ -9,6 +13,20 @@
 
 
 void print_to_98(int n)
+{
+	int k = n;
+
+	case_1(k);
+	case_2(k);
+	case_3(k);
+}
+/**
+ *case_1- prints up to 98 when n is less than 98.
+ *@n:argument passed-printing starts from n.
+ *
+ *Return:void.
+ */
+void case_1(int n)
 {
 	int i;
 
@@ -29,6 +47,16 @@ void print_to_98(int n)
 		}
 		printf("\n");
 	}
+}
+/**
+ *case_2- prints up to 98 when n is greater than 98.
+ *@n:Argument passed, printing starts here.
+ *Return-void.
+ */
+void case_2(int n)
+{
+	int i;
+
 	if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
@@ -46,6 +74,14 @@ void print_to_98(int n)
 		}
 		printf("\n");
 	}
+}
+/**
+ *case_3-prints  98.
+ *@n:argument passed must be 98.
+ *Return 0.
+ */
+void case_3(int n)
+{
 	if (n == 98)
 	{
 		printf("%d", n);
