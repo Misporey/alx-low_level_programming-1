7d1be679ce11667900697563939dac3064bcd586
david-kimengu <davidmutune200@gmail.com>
2020-09-24 17:01:52 +0000

    Add task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,13 @@ void print_diagonal(int n)
 {
 
 	int  m, p;
-	if (n == 0)
+
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else{
+	else
+	{
 		for (m = 0; m < n; m++)
 		{
 			for (p = 0; p < m; p++)
