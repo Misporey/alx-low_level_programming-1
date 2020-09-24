b63ded4fc316afa7bfc44a90b597b367d6af033f
david-kimengu <davidmutune200@gmail.com>
2020-09-24 17:00:17 +0000

    Add task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *print_diagonal-prints a diagnal line on the console.
+ *@n:length of diagonal.
+ *
+ *Return:void.
+ */
+
+void print_diagonal(int n)
+{
+
+	int  m, p;
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else{
+		for (m = 0; m < n; m++)
+		{
+			for (p = 0; p < m; p++)
+			{
+			_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
