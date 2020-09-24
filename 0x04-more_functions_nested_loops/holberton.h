d98e0c99ac0e870b46e18febf4df67f2589f4093
david-kimengu <davidmutune200@gmail.com>
2020-09-24 12:14:41 +0000

    Task 3

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-extern int _isupper(int c);
-extern int _isdigit(int c);
-extern int mul(int a, int b);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 #endif
