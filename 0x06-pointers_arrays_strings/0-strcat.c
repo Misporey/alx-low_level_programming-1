86fe543e10e4616961ef100f50fbfd4670c1a1b2
david-kimengu <davidmutune200@gmail.com>
2020-10-01 12:22:11 +0000

    Added task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ *_strcat-concatenates a string.
+ *@dest:destination string.
+ *@src:source string.
+ *
+ *Return:Pointer to the cancated string.
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	char i, j = 0, m = 0, k, size, n, r = 0;
+/*get the size of the dest string*/
+	while (i != '\0')
+	{
+		i = *(dest + j);
+		j++;
+	}
+/*get size of the src string.*/
+	while (k != '\0')
+	{
+		k = *(src + m);
+		m++;
+	}
+	size = j + m;
+
+	for (n = 0; n < size; n++)
+	{
+		if (n <= j)
+		{
+			/**(dest + n) = *(dest + n);*/
+		}
+		else
+		{
+			*(dest + n) = *(src + r);
+			r++;
+		}
+	}
+	return (dest);
+
+
+
+}
