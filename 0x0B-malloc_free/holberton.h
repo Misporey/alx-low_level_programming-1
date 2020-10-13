198dc0bb9ae05a9664c5cce75496509556b02a20
david-kimengu <davidmutune200@gmail.com>
2020-10-13 14:35:32 +0000

    Add task 2

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -4,4 +4,7 @@ char *create_array(unsigned int size, char c);
 void _memset(char *s, char b, unsigned int n);
 char *_strdup(char *str);
 int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
 #endif
