dc22e4b8cf1faf1526ef002c1529455d642fe2b4
david-kimengu <davidmutune200@gmail.com>
2020-10-15 18:39:37 +0000

    Add task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_realloc-Reallocates a block of memory.
+ *@ptr:pointer to the memory previously allocated
+ *@old_size:size of the old memory block.
+ *@new_size:New size of the block of memory.
+ *
+ *Return:Pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newptr;
+
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+		if (newptr == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		free(ptr);
+		return (newptr);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+		_memcpy(newptr, ptr, old_size);
+	free(ptr);
+	return (newptr);
+}
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: dest pointer
+ * @src: source pointer
+ * @n: size of bytes
+ * Return: pointer to memory area.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + j);
+		i += 1;
+		j += 1;
+	}
+	return (dest);
+}
