af5ae2dff9c55fc0be1f5099d0dbd3541424e6a4
david-kimengu <davidmutune200@gmail.com>
2020-10-15 17:52:46 +0000

    Add task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,18 +11,35 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	void *ptr;
 
-	if ((nmemb || size) == 0)
-	{
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	}
-/*Reserve memory*/
+
 	ptr = malloc(nmemb * size);
-/*check if malloc failed.*/
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+	_memset(ptr, 0, nmemb * size);
 	return (ptr);
+}
+/**
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: memory area to return
+ * @b: constant byte
+ * @n: size of bytes
+ * Return: pointer to memory area.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
 }
