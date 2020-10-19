5c7d54dd02419ca733fb39ea1d7d39eadc461134
david-kimengu <davidmutune200@gmail.com>
2020-10-19 12:37:53 +0000

    Add task zero

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_
+#define _dog_
+/**
+ *struct dog-basic struct definition.
+ *@name:First member.
+ *@age:Second member.
+ *@owner:Third member.
+ *
+ *Description:Define a basic struct and assign some properties.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
