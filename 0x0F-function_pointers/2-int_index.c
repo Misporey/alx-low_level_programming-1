d80aa3c23514db1cfd52e4bde4b3cd71d1094edb
david-kimengu <davidmutune200@gmail.com>
2020-10-21 18:37:27 +0000

    Add task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+#include <stdbool.h>
+/**
+ *int_index-Searches for an integer.
+ *@array:the array to search from.
+ *@size:Size of the array.
+ *@cmp:Pointer to the fxn that checks for an int.
+ *
+ *Return:index of the found index.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j;
+	bool k;
+
+	if (size <= 0)
+	{
+		return (0);
+	}
+
+	for (j = 0; j < size; j++)
+	{
+		k = cmp(array[j]);
+		if (k == true)
+		{
+			return (j);
+		}
+	}
+
+	return (-1);
+}
