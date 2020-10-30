bca1d6218bedc6fd02e1322704d1d2370399cd47
david-kimengu <davidmutune200@gmail.com>
2020-10-30 16:49:54 +0000

    Remove system interrupts

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,20 @@
 section .text
 	global main
-main:
-	mov edx,len
-	mov ecx,msg
-	mov ebx,1
-	mov eax,4
-	int 0x80
-	mov eax,1
-	int 0x80
+	extern printf
+
+
 section .data
-	msg db 'Hello, Holberton', 0xa
-	len equ $ - msg
+	format db	"%s", 10, 0
+	message db	"Hello, Holberton", 0 ;Oxa-represents newline(\n).
+
+main:
+	push	rbp		;points to the base of th current stack frame.
+	;; rbp is the register base pointer.This instruction will push the
+	;; value of rbp into the stack
+	mov	rsi, message	;rsi register source index.
+	;; passes, passess second argument to functions. it is the
+	;; register destination index.
+	mov	rdi, format
+	call 	printf
+	pop	rbp		;register base pointer.
+	ret			;Transfer program execution back to main program
