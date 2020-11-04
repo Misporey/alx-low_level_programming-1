c101c5b9903eed010d71281eddad1ce990250240
david-kimengu <davidmutune200@gmail.com>
2020-11-04 17:17:57 +0000

    Iterate through list on condition

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,13 +8,19 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	listint_t *temp = head;
 
-	for (i = 0; i < index; i++)
+	while (i < index && temp != NULL)
 	{
 		temp = temp->next;
+		i++;
 	}
+
+/*	for (i = 0; i < index; i++)*/
+/*	{*/
+/*		temp = temp->next;*/
+/*		}*/
 	if (temp == NULL)
 	{
 		return (NULL);
