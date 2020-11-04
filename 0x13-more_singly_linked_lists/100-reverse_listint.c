387c41ccb1b40a25f8ad5bf8d970dda6ac88a07b
david-kimengu <davidmutune200@gmail.com>
2020-11-04 19:46:41 +0000

    Limit only to 2 variables declared

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,21 +4,22 @@
  *@head:Pointer to a pointer that points to the head node.
  *Return:The head node.
  */
+
 listint_t *reverse_listint(listint_t **head)
 {
-	{
-		listint_t *prev = NULL;
-		listint_t *current = *head;
-		listint_t *next = NULL;
 
-		while (current != NULL)
-		{
-			next = current->next;
-			current->next = prev;
-			prev = current;
-			current = next;
-		}
-		*head = prev;
-	}
-	return (*head);
+listint_t *next;
+listint_t *previous = NULL;
+
+while ((*head)->next != NULL)
+{
+next = (*head)->next;
+(*head)->next = previous;
+previous = *head;
+*head = next;
+}
+
+(*head)->next = previous;
+
+return (*head);
 }
