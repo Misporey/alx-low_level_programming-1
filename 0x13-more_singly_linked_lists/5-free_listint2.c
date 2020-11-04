3b676596dc2abb661a67633446d1a17fde180459
david-kimengu <davidmutune200@gmail.com>
2020-11-04 09:49:58 +0000

    Use correct way to reference pointer to a pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,27 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- *free_listint2-Frees a list by setting the head to null.
+ *free_listint2-Frees memory occupied by a list.
  *@head:Pointer to a pointer that points to the head of a linked list.
  *Return:Nothing.
  */
 void free_listint2(listint_t **head)
 {
-	*head = NULL;
+	listint_t *temp;
+	if (*head == NULL)
+	{
+		free(*head);
+	}
+
+	else
+	{
+
+		while (*head != NULL)
+		{
+			temp = (*head);
+			(*head) = (*head)->next;
+			free(temp);
+		}
+		(*head) = NULL;
+	}
 }
