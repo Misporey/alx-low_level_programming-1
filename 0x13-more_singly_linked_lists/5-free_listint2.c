50b0f397df01467d6417d429c0054c4a6737b014
david-kimengu <davidmutune200@gmail.com>
2020-11-04 07:38:05 +0000

    Add basic tasks 0-5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ *free_listint2-Frees a list by setting the head to null.
+ *@head:Pointer to a pointer that points to the head of a linked list.
+ *Return:Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	*head = NULL;
+}
