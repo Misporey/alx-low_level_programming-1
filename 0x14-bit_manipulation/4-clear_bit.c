721cb4c11b07ec45b9e840ba45335c1e1940911b
david-kimengu <davidmutune200@gmail.com>
2020-11-05 18:34:18 +0000

    Add case for -1 when bit  setting fails

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,18 @@
 #include "holberton.h"
+/**
+ *clear_bit-Set a bit at a given index to zero.
+ *@n:Pointer to an int.
+ *@index:Bit position to set to zero.
+ *Return:1-success, -1-fail.
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-
 	int mask = 1 << index;
+
+	if (index > 64)
+	{
+		return (-1);
+	}
 	*n = (*n & ~mask) | ((0 << index) & mask);
 	return (1);
 }
