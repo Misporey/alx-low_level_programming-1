a6190c7e709bef767b1867e7804229ea2b967c0b
david-kimengu <davidmutune200@gmail.com>
2020-11-05 20:35:05 +0000

    Add advanced task 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *get_endianness-Checks endianness of a machine.
+ *Return:Nothing.
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
