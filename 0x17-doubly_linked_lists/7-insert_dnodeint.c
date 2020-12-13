8b4cb25a0de928f6be8428485a6c24dc6712938e
david-kimengu <davidmutune200@gmail.com>
2020-12-13 17:54:24 +0000

    Add task 7, insert node at index n in a linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,9 +10,12 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
+	unsigned int len_of_d = sizeof_dlist(*h);
 	dlistint_t *temp = *h;
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
 
+	if (idx > len_of_d)
+		return (NULL);
 	if (newNode == NULL)
 	{
 		return (NULL);
@@ -33,4 +36,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newNode->prev = temp;
 	temp->next = newNode;
 	return (newNode);
+}
+
+/**
+ *sizeof_dlist - Finds the size of a doubly linked list.
+ *@head: Pointer to the head of a doubly linked list.
+ *Return: Size of the doubly linked list.
+ */
+
+int sizeof_dlist(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int i = 0;
+
+	while (temp != NULL)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i - 1);
 }
