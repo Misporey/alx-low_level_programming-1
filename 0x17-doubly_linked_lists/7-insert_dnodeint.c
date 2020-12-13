377eae8b81562699ed0acfeeedf61257ad554683
david-kimengu <davidmutune200@gmail.com>
2020-12-13 18:31:55 +0000

    Handle last index case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			temp = temp->next;
 		}
+		if (temp == NULL)
+		{
+			return (add_dnodeint_end(h, n));
+		}
 		newNode->next = temp->next;
 		newNode->prev = temp;
 		temp->next = newNode;
