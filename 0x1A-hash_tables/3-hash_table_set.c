7c779bd58a3678396fc1c37a20d2fa5b78e1f1fb
david-kimengu <davidmutune200@gmail.com>
2021-01-17 08:52:23 +0000

    Add task 3, insert into hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new_node;
 	char *value_c;
-	unsigned long int i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
