85d4dad93e72413fa6fea1e11b9eead08c7fc9b2
david-kimengu <davidmutune200@gmail.com>
2021-01-17 09:49:21 +0000

    Add task 3, insert into hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,19 +39,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	if (ht->array[index] != NULL)
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-		return (1);
-	}
 	if (strcmp(ht->array[index]->key, key) == 0)
 	{
-		free(ht->array[index]->value);
 		ht->array[index]->value = value_c;
 		return (1);
 	}
-
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
