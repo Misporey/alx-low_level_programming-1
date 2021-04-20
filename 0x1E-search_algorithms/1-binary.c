6eaf9ebb9a83c504d30f766f2c7ff1237cc9870d
kimengu-david <davidmutune200@gmail.com>
2021-04-20 14:47:14 +0000

    Binary sort algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,7 +5,7 @@
  *@array:Pointer to the first element of the array to search in.
  *@size: Size of the array to the array.
  *@value: The actual element to search for in the arrray.
- *Return: -1 if value not found, else index of the found element.
+ *Return: -1 if value not found, else index of the found element
  */
 
 int binary_search(int *array, size_t size, int value)
