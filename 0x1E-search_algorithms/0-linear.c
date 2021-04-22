b38e35e1f7d8a78afb5c8091f31b87bb55f2022a
kimengu-david <davidmutune200@gmail.com>
2021-04-22 19:06:19 +0000

    Check if array is Null

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,13 +13,12 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int size_ar = (int) size;
-	int i;
+	size_t i;
 
 	if (array == NULL)
-		return (-1)
+		return (-1);
 
-	for (i = 0; i < size_ar; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
