6ca5de69848a81784f5565f0e37bdfe0f2336620
kimengu-david <davidmutune200@gmail.com>
2021-05-24 21:41:53 +0000

    Print to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	char *p = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-	write(2, p, 59);
 	return (1);
 
 }
